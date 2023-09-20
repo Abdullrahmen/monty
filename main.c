465de8e88e3304e6aacfa2b59c7b21f97c9c1227
Abdullrahmen <abdullrahmen2003@gmail.com>
2023-09-20 22:15:10 +0000

    Add and fix some tasks

--- main.c
+++ main.c
@@ -27,7 +27,7 @@ int main(int argc, char **argv)
 	file_ptr = fopen(argv[1], "r");
 	if (!file_ptr)
 	{
-		fprintf(stderr, "Error: Can't open file %s\n", argv[0]);
+		fprintf(stderr, "Error: Can't open file %s\n", argv[1]);
 		return (EXIT_FAILURE);
 	}
 	init_instructions(instructions);
